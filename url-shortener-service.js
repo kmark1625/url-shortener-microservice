cf8a564b5ab36158aaae48f980e232815efa7e5c
Kevin Mark <kmark1625@gmail.com>
2016-11-28 23:42:44 -0600

    Update GET request handler to use REGEX to handle /

--- url-shortener-service.js
+++ url-shortener-service.js
@@ -1,9 +1,9 @@
 function UrlShortenerService() { }
 
 UrlShortenerService.prototype.shorten = function(url) {
-    if (isValid(url)) {
-        // TODO
-        return {};
+    if (isValidUrl(url)) {
+        var shortUrl = "https://localhost:3000/" + 1
+        return {"original_url": url, "short_url": shortUrl};
     } else {
         var response = {"error": "Wrong url format, make sure you have a valid protocol and real site."};
     } 
